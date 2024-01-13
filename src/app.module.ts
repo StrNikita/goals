3cd775d568dcc033ab1e5aa17e1eea6f0a9b69f8
Никита Стратий <nikita@Noutbuk-Nikita.local>
2024-01-13 19:36:14 +0300

    add nestjs and docker file for db

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { GoalModule } from './goal/goal.module';
+import { BalanceModule } from './balance/balance.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      migrations: [__dirname + '/migrations/**/*.{js,ts}'],
+    }),
+    GoalModule,
+    BalanceModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
